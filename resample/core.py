93858587c3d756509dba05129150729656a861d5
Brian McFee <brian.mcfee@nyu.edu>
2016-04-08 16:52:42 -0400

    updating docstrings

--- resample/core.py
+++ resample/core.py
@@ -8,6 +8,9 @@ import six
 
 from .resample import resample_f
 
+__all__ = ['resample']
+
+
 def make_window(num_zeros, num_table, window, sr_orig, sr_new, rolloff=0.95):
     '''Construct the interpolation window'''
 
@@ -28,6 +31,44 @@ def make_window(num_zeros, num_table, window, sr_orig, sr_new, rolloff=0.95):
 
 def resample(x, sr_orig, sr_new, num_zeros=31, precision=9, window=None, axis=-1):
     '''Resample a signal x
+
+    Parameters
+    ----------
+    x : np.ndarray
+        The input signal(s) to resample
+
+    sr_orig : int > 0
+        The sampling rate of x
+
+    sr_new : int > 0
+        The target sampling rate of the output signal(s)
+
+    num_zeros : int > 0
+        Number of zero-crossings to use in the low-pass filter.
+        Larger values give higher accuracy, but increase running time.
+
+    precision : int > 0
+        Number of bits to use for the filter table index.
+
+    window : callable or None
+        A window function to taper the low-pass filter.
+        By default, uses `scipy.signal.blackmanharris`.
+
+        .. seealso:: scipy.signal
+
+    axis : int
+        The target axis along which to resample `x`
+
+    Returns
+    -------
+    y : np.ndarray, dtype=np.float32
+        `x` resampled to `sr_new`
+
+    Raises
+    ------
+    TypeError
+        if `window` is not callable or `None`
+
     '''
     if window is None:
         window = scipy.signal.blackmanharris
@@ -40,7 +81,7 @@ def resample(x, sr_orig, sr_new, num_zeros=31, precision=9, window=None, axis=-1
     shape = list(x.shape)
     shape[axis] = int(shape[axis] * sample_ratio)
 
-    y = np.zeros(shape, dtype=x.dtype)
+    y = np.zeros(shape, dtype=np.float32)
 
     num_table = 2**precision
     interp_win = make_window(num_zeros, num_table, window, sr_orig, sr_new)
@@ -49,7 +90,7 @@ def resample(x, sr_orig, sr_new, num_zeros=31, precision=9, window=None, axis=-1
     interp_delta[:-1] = np.diff(interp_win)
 
     # Construct 2d views of the data with the resampling axis on the first dimension
-    x_2d = x.swapaxes(0, axis).reshape((x.shape[axis], -1))
+    x_2d = x.swapaxes(0, axis).reshape((x.shape[axis], -1)).astype(np.float32)
     y_2d = y.swapaxes(0, axis).reshape((y.shape[axis], -1))
     resample_f(x_2d, y_2d, sample_ratio, interp_win, interp_delta, num_table)
 
