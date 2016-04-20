63b8f03a88c10f117372c87a8b1388f1fe9ce49e
Brian McFee <brian.mcfee@nyu.edu>
2016-04-20 16:55:33 -0400

    transitioning install script to setuptools

--- setup.py
+++ setup.py
@@ -1,109 +1,36 @@
-# Setup script for resampy
-# Adapted from cython template setup.py script (BSD-licensed)
+from setuptools import setup
+from setuptools.extension import Extension
 
-import io
-import os
-import re
-import sys
-import subprocess
+import imp
 
+version = imp.load_source('resampy.version', 'resampy/version.py')
 
-PACKAGE_NAME = 'resampy'
-DESCRIPTION = "resampy: Efficient signal resampling"
-LONG_DESCRIPTION = DESCRIPTION
-AUTHOR = "Brian McFee"
-AUTHOR_EMAIL = "brian.mcfee@nyu.edu"
-URL = 'https://github.com/bmcfee/resampy'
-DOWNLOAD_URL = 'https://github.com/bmcfee/resampy'
-LICENSE = 'ISC'
-
-
-def version(package, encoding='utf-8'):
-    """Obtain the packge version from a python file e.g. pkg/__init__.py
-
-    See <https://packaging.python.org/en/latest/single_source_version.html>.
-    """
-    path = os.path.join(os.path.dirname(__file__), package, 'version.py')
-    with io.open(path, encoding=encoding) as fp:
-        version_info = fp.read()
-    version_match = re.search(r"""^version = ['"]([^'"]*)['"]""",
-                              version_info, re.M)
-    if not version_match:
-        raise RuntimeError("Unable to find version string.")
-    return version_match.group(1)
-
-
-def generate_cython(package):
-    """Cythonize all sources in the package"""
-    cwd = os.path.abspath(os.path.dirname(__file__))
-    print("Cythonizing sources")
-    p = subprocess.call([sys.executable,
-                         os.path.join(cwd, 'tools', 'cythonize.py'),
-                         package],
-                        cwd=cwd)
-    if p != 0:
-        raise RuntimeError("Running cythonize failed!")
-
-
-def configuration(parent_package='',top_path=None):
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration(None, parent_package, top_path)
-    config.set_options(ignore_setup_xxx_py=True,
-                       assume_default_configuration=True,
-                       delegate_options_to_subpackages=True,
-                       quiet=True)
-
-    config.add_subpackage(PACKAGE_NAME)
-
-    return config
-
-
-def setup_package():
-    from numpy.distutils.core import setup
-
-    old_path = os.getcwd()
-    local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
-    src_path = local_path
-
-    os.chdir(local_path)
-    sys.path.insert(0, local_path)
-
-    # Run build
-    old_path = os.getcwd()
-    os.chdir(src_path)
-    sys.path.insert(0, src_path)
-
-    cwd = os.path.abspath(os.path.dirname(__file__))
-    if not os.path.exists(os.path.join(cwd, 'PKG-INFO')):
-        # Generate Cython sources, unless building from source release
-        generate_cython(PACKAGE_NAME)
-
-    try:
-        setup(name=PACKAGE_NAME,
-              author=AUTHOR,
-              author_email=AUTHOR_EMAIL,
-              url=URL,
-              download_url=DOWNLOAD_URL,
-              description=DESCRIPTION,
-              long_description=LONG_DESCRIPTION,
-              version=version(PACKAGE_NAME),
-              license=LICENSE,
-              configuration=configuration,
-              classifiers=[
-                'Development Status :: 3 - Alpha',
-                'Environment :: Console',
-                'Intended Audience :: Science/Research',
-                'License :: OSI Approved :: ISC License (ISCL)',
-                'Natural Language :: English',
-                'Programming Language :: Python :: 2.7',
-                'Programming Language :: Python :: 3.4',
-                'Programming Language :: Python :: 3.5'])
-    finally:
-        del sys.path[0]
-        os.chdir(old_path)
-
-    return
-
-
-if __name__ == '__main__':
-    setup_package()
+setup(
+    author="Brian McFee",
+    author_email="brian.mcfee@nyu.edu",
+    name='resampy',
+    version=version.version,
+    url='https://github.com/bmcfee/resampy',
+    download_url='https://github.com/bmcfee/resampy/releases',
+    description='Efficient signal resampling',
+    license='ISC',
+    ext_modules=[Extension("resampy.resample",
+                           ["resampy/resample.c"])],
+    packages=['resampy'],
+    package_data={'resampy': ['data/*']},
+    install_requires=[
+        'numpy>=1.10',
+        'scipy>=0.13',
+        'six>=1.3',
+        'Cython>=0.21'],
+    classifiers=[
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+) 
