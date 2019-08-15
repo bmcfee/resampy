1063de3283cdef31c272dc8c0380e2b13d193cff
Brian McFee <brian.mcfee@nyu.edu>
2019-08-15 19:19:04 -0400

    Fixed some build issues, pinned pytest version, added 3.7, removed 3.4 from build matrix

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
             'numpydoc',
         ],
         'tests': [
-            'pytest',
+            'pytest < 4',
             'pytest-cov',
         ],
     },
@@ -42,8 +42,8 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
