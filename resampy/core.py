3a6ea45d19d562cb7158c9bac52f7c72fa8e70ab
Antonio Valentino <antonio.valentino@tiscali.it>
2022-06-11 19:38:31 +0200

    Typo

--- resampy/core.py
+++ resampy/core.py
@@ -198,7 +198,7 @@ def resample_nu(x, sr_orig, t_out, axis=-1, filter='kaiser_best', **kwargs):
     if t_out.ndim != 1:
         raise ValueError('Invalide t_out shape ({}), 1D array expected'.format(t_out.shape))
     if np.min(t_out) < 0 or np.max(t_out) > (x.shape[axis] - 1) / sr_orig:
-        raise ValueError('Output domain [{}, {}] exceedes the data domain [0, {}]'.format(
+        raise ValueError('Output domain [{}, {}] exceeds the data domain [0, {}]'.format(
             np.min(t_out), np.max(t_out), (x.shape[axis] - 1) / sr_orig))
 
     # Set up the output shape
