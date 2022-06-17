e64ecf4dc1950cc847ca522f0bcb2c35b5dec49a
Brian McFee <brian.mcfee@nyu.edu>
2022-06-17 07:05:01 -0400

    dropped python 3.5 as well

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Multimedia :: Sound/Audio :: Analysis",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
