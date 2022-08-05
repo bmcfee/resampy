87255f067b8a2506fe7c9d15e62c8c1be1ba48cd
Brian McFee <brian.mcfee@nyu.edu>
2022-08-05 11:33:24 -0400

    reverted parallel=False by default, fixes #107

--- resampy/core.py
+++ resampy/core.py
@@ -12,7 +12,7 @@ __all__ = ["resample", "resample_nu"]
 
 
 def resample(
-    x, sr_orig, sr_new, axis=-1, filter="kaiser_best", parallel=True, **kwargs
+    x, sr_orig, sr_new, axis=-1, filter="kaiser_best", parallel=False, **kwargs
 ):
     """Resample a signal x from sr_orig to sr_new along a given axis.
 
@@ -160,7 +160,7 @@ def resample(
 
 
 def resample_nu(
-    x, sr_orig, t_out, axis=-1, filter="kaiser_best", parallel=True, **kwargs
+    x, sr_orig, t_out, axis=-1, filter="kaiser_best", parallel=False, **kwargs
 ):
     """Interpolate a signal x at specified positions (t_out) along a given axis.
 
